3cc4bac028c7411d3a961ba18d2807b0aa7e7175
Sergiusz Gromadziński <serekgrom@gmail.com>
2024-04-21 21:00:20 +0200

    Authentication applied

---
+++ components/ui/navbar/Navbar.tsx
@@ -0,0 +1,76 @@
+'use client'
+
+import { useState } from 'react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import Image from 'next/image'
+import { UserButton, useUser } from '@clerk/nextjs'
+import { IoMenu, IoClose } from 'react-icons/io5'
+
+const linksList: {
+  label: string
+  path: string
+  auth: boolean
+}[] = [
+  {
+    label: 'Groups',
+    path: '/groups',
+    auth: true,
+  },
+  {
+    label: 'User panel',
+    path: '/user_panel',
+    auth: true,
+  },
+  {
+    label: 'Sign in',
+    path: '/sign-in',
+    auth: false,
+  },
+  {
+    label: 'Sign up',
+    path: '/sign-up',
+    auth: false,
+  },
+]
+
+export function Navbar() {
+  const pathname = usePathname()
+  const { isSignedIn } = useUser()
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false)
+
+  return (
+    <nav className="sm:border-primary my-3 flex w-full flex-wrap items-center justify-between gap-6 rounded-3xl px-6 py-2.5 sm:w-auto sm:border-2">
+      <Link href="/" className="font-bold sm:border-r sm:pr-6">
+        NoteZ
+      </Link>
+      <div className="flex gap-3 sm:order-3 sm:flex-row-reverse">
+        {isSignedIn && <UserButton afterSignOutUrl="/" />}
+
+        <button
+          className="text-4xl sm:hidden"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          {isMenuOpen ? <IoClose /> : <IoMenu />}
+        </button>
+      </div>
+      <ul
+        className={` ${isMenuOpen ? 'flex' : 'hidden'} h-screen w-full flex-col gap-8 sm:flex sm:h-auto sm:w-auto sm:flex-row`}
+      >
+        {linksList.map(
+          (el, index) =>
+            el.auth === isSignedIn && (
+              <li key={index} onClick={() => setIsMenuOpen(false)}>
+                <Link
+                  href={el.path}
+                  className={`${el.path == pathname && 'text-primary'} hover:text-primary text-sm font-bold transition-all duration-300`}
+                >
+                  {el.label}
+                </Link>
+              </li>
+            )
+        )}
+      </ul>
+    </nav>
+  )
+}
